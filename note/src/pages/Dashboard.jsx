53fe3d73667e144598e2cc50f3b1d42759fbb09c
KRISHNA <kraggr2909@gmail.com>
2025-08-24 09:53:06 +0530

    dashboard

--- note/src/pages/Dashboard.jsx
+++ note/src/pages/Dashboard.jsx
@@ -4,28 +4,31 @@ import { auth } from '../firebase/firebase'
 import authService from '../firebase/user';
 
 function Dashboard() {
-  const [user, setUser] = useState(null);
+    const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    const unsub = onAuthStateChanged(auth, (u) => {
-      setUser(u);
-    });
-    return unsub; // cleanup
-  }, []);
+    useEffect(() => {
+        const unsub = onAuthStateChanged(auth, (u) => {
+            setUser(u);
+        });
+        return unsub; // cleanup
+    }, []);
 
-  const handleLogout = () => {
-    authService.logout();
-  }
+    const handleLogout = () => {
+        authService.logout();
+    }
 
-  if (!user) return <p className="text-white">Loading...</p>;
+    if (!user) return <p className="text-white">Loading...</p>;
 
-  return (
-    <div className='text-white'>
-      <p>Welcome {user.displayName}</p>
-
-      <button onClick={handleLogout}>logout</button>
-    </div>
-  )
+    return (
+        <div className='text-white flex w-full h-screen'>
+            <div className='flex-1/7 bg-red-100'>
+                <p>Welcome {user.displayName}</p>
+            </div>
+            <div className='flex-6/7 bg-red-400'>
+                <button onClick={handleLogout}>logout</button>
+            </div>
+        </div>
+    )
 }
 
 export default Dashboard;
